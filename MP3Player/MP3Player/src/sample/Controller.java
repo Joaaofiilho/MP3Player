2958e11180684f93d55d3b3a2836be52f404fc15
João Ferreira <joaot10112@gmail.com>
2017-11-14 15:50:49 -0200

    Adicionados os botões na interface

---
+++ MP3Player/MP3Player/src/sample/Controller.java
@@ -0,0 +1,35 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class Controller {
+
+    @FXML
+    public Button btnPlayPause;
+    public Button btnStop;
+    public Button btnForward;
+    public Button btnBackward;
+
+    public Slider sldProgressBar;
+    public Slider sldVolumeBar;
+
+    public AnchorPane ancpBackground;
+
+    public void btnPlayPauseOnAction(ActionEvent event){
+
+    }
+    public void btnStopOnAction(ActionEvent event){
+
+    }
+    public void btnForwardOnAction(ActionEvent event){
+
+    }
+    public void btnBackwardOnAction(ActionEvent event){
+
+    }
+
+
+}
