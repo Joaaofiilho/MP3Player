8cf54f76ad62948b0bc75e84e9692bb30784c3e8
RubensDiniz <rubensadiniz@edu.unifor.br>
2017-11-14 11:07:58 -0200

    Adicionados botoes

--- MP3Player/src/sample/Controller.java
+++ MP3Player/src/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class Controller {
+
+    @FXML
+    public Button btnPlayPause;
+    public Button btnStop;
+    public Button btnForward;
+    public Button btnBackward;
+
+    public Slider sldProgressBar;
+    public Slider sldVolumeBar;
+
+    public AnchorPane teste;
+
+    public void btnPlayPauseOnClick(ActionEvent event){
+
+    }
+    public void btnStopOnClick(ActionEvent event){
+
+    }
+    public void btnForwardClick(ActionEvent event){
+
+    }
+    public void btnBackwardOnClick(ActionEvent event){
+
+    }
+
+
 }
